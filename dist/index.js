ed47017e00704395fd226153ed4b2f2b1a400b49
khouari1 <karim.houari@genesis.global>
2022-01-05 12:15:22 +0000

    Update download from github

--- dist/index.js
+++ dist/index.js
@@ -484,17 +484,17 @@ try {
     switch (os.platform()) {
         case 'linux': {
             let client_url = `${base_url}/foundationdb-clients_${version}-1_amd64.deb`;
-            exec(`curl -O ${client_url}`);
+            exec(`curl -L -O ${client_url}`);
             exec(`sudo dpkg -i foundationdb-clients_${version}-1_amd64.deb`);
 
             let server_url = `${base_url}/foundationdb-server_${version}-1_amd64.deb`;
-            exec(`curl -O ${server_url}`);
+            exec(`curl -L -O ${server_url}`);
             exec(`sudo dpkg -i foundationdb-server_${version}-1_amd64.deb`);
             break;
         }
         case 'darwin': {
             let url = `${base_url}/FoundationDB-${version}.pkg`;
-            exec(`curl -O ${url}`);
+            exec(`curl -L -O ${url}`);
             exec(`sudo installer -pkg FoundationDB-${version}.pkg -target /`);
             break;
         }
